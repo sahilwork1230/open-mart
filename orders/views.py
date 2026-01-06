35df668348d5d8cbd2f6ef9b3573a402685ee1e7
sahilwork1230 <sahilwork1230@gmail.com>
2026-01-06 12:02:30 +0530

    create order view

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Order, OrderItem
+from django.shortcuts import redirect, get_object_or_404
+from cart.models import Cart
 
-# Create your views here.
+@login_required
+def create_order(request):
+    cart, _ = Cart.objects.get_or_create(user = request.user)
+
+    
+
+    # Safety check
+    if not cart.items.exists():
+        return redirect("view-cart")
+    
+    # Create Order ----- Sahil -----
+    order = Order.objects.create(
+        user = request.user,
+        total_price = cart.total_price()
+    )
+
+    # Copy cart items in Order object
+
+    for item in cart.items.all():
+        OrderItem.objects.create(
+            order = order,
+            product_name = item.product.title,
+            price = item.price,
+            quantity = item.quantity
+        )
+
+    # Clear cart AFTER successful order 
+    
+    cart.items.all().delete()
+    pass
+
+
+
