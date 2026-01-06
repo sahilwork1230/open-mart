06bec406e285aca2df9a70c1273e9dc1f567628e
sahilwork1230 <sahilwork1230@gmail.com>
2026-01-06 10:30:20 +0530

    create order models and minor changes in code

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete= models.PROTECT
+    )
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_paid = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id}"
+    
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order,
+        on_delete = models.CASCADE,
+        related_name= "items"
+    )
+    product_name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+
+    def subtotal(self):
+        return self.price * self.quantity
+    
+    
