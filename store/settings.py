73eeeb4733a4704ddeaa464fd06ef5507265183b
Sahil Kumar <sahilwork1230@gmail.com>
2026-03-02 11:41:19 +0530

    update gitignore and tailwind

--- store/settings.py
+++ store/settings.py
@@ -42,8 +42,13 @@ INSTALLED_APPS = [
     'cart',
     'orders',
     'phonenumber_field',
-    'django_browser_reload',
+    "tailwind",
+    "theme",
+    "django_browser_reload",
 ]
+
+TAILWIND_APP_NAME = 'theme'
+
 PHONENUMBER_DEFAULT_REGION = "IN"
 
 MIDDLEWARE = [
@@ -135,3 +140,5 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
